165c68734c042924bfa57b1f5655885aecd77408
Siddharth Agrawal <Sid@Sids-MacBook-Air.local>
2019-10-02 14:28:50 +0530

    Add High and Low Temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,9 @@ const forecast = (latitude, longitude, callback)=> {
         } else if(body.error) {
                callback('Unable to fetch the Location', undefined)
         } else {
+            
             callback(undefined,
-                body.daily.data[0].summary + ' It is currently ' +body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain.')
+                body.daily.data[0].summary + ' It is currently ' +body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain. The high today is '+body.daily.data[0].temperatureHigh+' and the low is '+body.daily.data[0].temperatureLow+' .')
 
         }
     })
